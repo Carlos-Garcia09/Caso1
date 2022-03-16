7b168701c301e1f479b99475351f4ac8405d4ce1
Carlos-Garcia09 <75263278+Carlos-Garcia09@users.noreply.github.com>
2022-03-15 21:29:29 -0600

    Se agregan librerias para el registro y loguin

--- webstore/views.py
+++ webstore/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render,redirect
-
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from .forms import *
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 
 # Create your views here.
 
