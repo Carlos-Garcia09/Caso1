517747f43e4887fecb0d8ace238d5ed4fccd3e0f
Carlos-Garcia09 <75263278+Carlos-Garcia09@users.noreply.github.com>
2022-03-15 23:19:04 -0600

    Agregamos la ruta para eliminar productos

--- webstore/urls.py
+++ webstore/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('apanel/',views.apanel, name='apanel'),
     path('product/<str:pk>/',views.product_page, name='product_page'),
     path('addproduct/',views.add_product, name='addproduct'),
+    path('delp/<str:pk>',views.del_product, name='del_product'),
 ]
