4a71827c3d1c67fc407f57e90ccc1fa41013a399
Carlos-Garcia09 <75263278+Carlos-Garcia09@users.noreply.github.com>
2022-03-15 23:08:43 -0600

    Creamos el modelo para los productos

--- webstore/models.py
+++ webstore/models.py
@@ -9,3 +9,25 @@ class Client(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    description = models.CharField(max_length=500, null=True)
+    author = models.CharField(max_length=50, null=True)
+    publisher = models.CharField(max_length=50, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    quantity = models.IntegerField(default=1)
+    genre = models.CharField(max_length=100, null=True)
+    selled = models.IntegerField(blank=True, null=True, default=0)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
