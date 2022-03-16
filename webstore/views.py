1c1e2f2fb2bd800d1c3d9aa72d70f79fe2ed05aa
Carlos-Garcia09 <75263278+Carlos-Garcia09@users.noreply.github.com>
2022-03-15 21:28:16 -0600

    Se crean las funciones de login y registro
    Esto va a ser que se vuelvan funcionales los registros y logueos

--- webstore/views.py
+++ webstore/views.py
@@ -8,3 +8,52 @@ def home(request):
     context = {'products':products}
     return render(request,'home.html',context)
 
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        context = {}
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username = username, password = password)
+            if user is not None:
+                try:
+                    login(request, user)
+                    return redirect('home')
+                except:
+                    messages.success(request,"Ha ocurrido un error, intenta de nuevo")
+                    return render(request,'login.html',context)
+            else:
+                messages.success(request,"Usuario no registrado")
+                return render(request,'login.html',context)
+
+        return render(request,'login.html',context)
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        client_form = addClientForm()
+
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            client_form = addClientForm(request.POST)
+            if form.is_valid() and client_form.is_valid():
+                user = form.save()
+                client = client_form.save(commit=False)
+                client.user = user
+                client.save()
+                username = form.cleaned_data.get('username')
+                messages.success(request,"Cuenta creada para exitosamente para " + username)
+                return redirect('login')
+
+        context = {'form':form}
+        return render(request,'register.html',context)
+
+@login_required(login_url='login')
