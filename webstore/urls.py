54dbf5231036414c7eb8c5f3b89361273055e93e
Carlos-Garcia09 <75263278+Carlos-Garcia09@users.noreply.github.com>
2022-03-15 21:47:38 -0600

    Se agrega la ruta para los no logueados

--- webstore/urls.py
+++ webstore/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/',views.user_login, name='login'),
     path('logout/',views.user_logout, name='logout'),
     path('register/',views.register, name='register'),
-    
+    path('guest/',views.guest, name='guest'),
 ]
